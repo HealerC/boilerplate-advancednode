488fd2521ee286aaee5b4377a6fcefce771fd0f1
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-22 17:59:51 +0100

    Authentication Strategies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const myDB = require('./connection');						// Connection to MONGODB Database
 const fccTesting = require('./freeCodeCamp/fcctesting.js');	// Testing
@@ -48,9 +49,19 @@ myDB(async client => {
   	myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
    		done(null, doc);
     });
-});
+  });
 
-
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User '+ username +' attempted to log in.');
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        if (password !== user.password) { return done(null, false); }
+        return done(null, user);
+      });
+    }
+  ));
   // Be sure to add this...
 }).catch(e => {
   app.route('/').get((req, res) => {
