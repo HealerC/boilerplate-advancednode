7933fc7bfb613c26805301642c8dcfc1aea5ec39
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-22 17:21:01 +0100

    Set up passport

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+
+const myDB = require('./connection');						// Connection to MONGODB Database
+const fccTesting = require('./freeCodeCamp/fcctesting.js');	// Testing
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  	secret: process.env.SESSION_SECRET,
+  	resave: true,
+	saveUninitialized: true,
+  	cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'pug');		// Template engine
 const pugIndex = __dirname + '/views/pug';
