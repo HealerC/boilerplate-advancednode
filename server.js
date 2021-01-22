fa68f431dec0d8157ffd59a954cf4451667e2d8d
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-22 17:50:31 +0100

    Implement Serialization of a Passport user

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const myDB = require('./connection');						// Connection to MONGODB Database
 const fccTesting = require('./freeCodeCamp/fcctesting.js');	// Testing
@@ -25,10 +26,40 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.set('view engine', 'pug');		// Template engine
-const pugIndex = __dirname + '/views/pug';
-app.route('/').get((req, res) => {
-  res.render(pugIndex, {title: "Hello", message: "Please login"});
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    //Change the response to render the Pug template
+    res.render(__dirname + '/views/pug', {
+      title: 'Connected to Database',
+      message: 'Please login'
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+  	done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+  	myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+   		done(null, doc);
+    });
 });
+
+
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('pug', { title: e, message: 'Unable to login' });
+  });
+});
+// app.listen out here...
+
+
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
