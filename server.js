987acfa14a26eee6d6ec1639cfaf231d62b9beed
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-27 00:18:22 +0100

    Authentication with Socket.IO

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const passport = require('passport');
+
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+
+const passportSocketIo = require('passport.socketio');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
 
 const myDB = require('./connection');						// Connection to MONGODB Database
 const fccTesting = require('./freeCodeCamp/fcctesting.js');	// Testing
@@ -30,19 +35,48 @@ app.use(passport.session());
 
 app.set('view engine', 'pug');		// Template engine
 
+
+
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
   routes(app, myDataBase);
   auth(app, myDataBase);
 
+
+  const URI = process.env.MONGO_URI;
+  const store = new MongoStore({ url: URI });
+  
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  );
+
+  function onAuthorizeSuccess(data, accept) {
+    console.log('successful connection to socket.io');
+  
+    accept(null, true);
+  }
+  
+  function onAuthorizeFail(data, message, error, accept) {
+    if (error) throw new Error(message);
+    console.log('failed connection to socket.io:', message);
+    accept(null, false);
+  }
+
+  
   /* Emit from server to io */
   let currentUsers = 0;
 
-
-
   io.on('connection', socket => {
   	console.log("A user has connected");
-  	
+  	console.log("user " + socket.request.user.name + ' connected.');
+
   	++currentUsers;
   	io.emit('user count', currentUsers);
 
